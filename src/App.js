81d7812e60c3c8b41f31a22a4ec55d70556511e4
saketv2 <saketv2@illinois.edu>
2020-12-31 20:23:28 -0600

    fix playing screen

--- src/App.js
+++ src/App.js
@@ -1,155 +1,165 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import hash from "./hash";
-import Form from './components/Form';
+import Form from "./components/Form";
 import * as $ from "jquery";
 import Player from "./Player";
-
 
 /**
  * Spotify API Read
  */
-export const authEndpoint = 'https://accounts.spotify.com/authorize';
+export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 const clientId = "df010c4f38ab4947aabe303af43358d1";
 const redirectUri = "http://localhost:3000";
-const scopes = [
-  "user-read-currently-playing",
-  "user-read-playback-state",
-];
-
+const scopes = ["user-read-currently-playing", "user-read-playback-state"];
 
 // make this code more modular
 // break the rectangles into a function or component
 // try to add more components
-class App extends Component{
-  constructor(props){
+class App extends Component {
+  constructor(props) {
     super(props);
     this.state = {
       rectangles: [],
       // fix the colors later
-      colors: ['red', 'orange', 'yellow', 'green', 'blue', 
-      'indigo', 'violet', 'red', 'orange', 'yellow', 'green', 
-      'blue', 'indigo', 'violet', 'indigo', 'violet'],
+      colors: [
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "indigo",
+        "violet",
+        "indigo",
+        "violet",
+      ],
       token: null,
       item: {
-      album: {
-        images: [{ url: "" }]
+        album: {
+          images: [{ url: "" }],
+        },
+        name: "",
+        artists: [{ name: "" }],
+        duration_ms: 0,
       },
-      name: "",
-      artists: [{ name: "" }],
-      duration_ms:0,
-    },
       is_playing: "Paused",
-      progress_ms: 0
+      progress_ms: 0,
     };
     this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
   }
-// setup the array of rectangles
-componentDidMount(){
-  let _token = hash.access_token;
-  if (_token) {
-    // Set token
-    this.setState({
-      token: _token
-    });
-  }
+  // setup the array of rectangles
+  componentDidMount() {
+    let _token = hash.access_token;
+    if (_token) {
+      // Set token
+      this.setState({
+        token: _token,
+      });
+      this.getCurrentlyPlaying(_token);
+    }
     this.randomArray();
   }
 
-getCurrentlyPlaying(token) {
-  // Make a call using the token
-  $.ajax({
-    url: "https://api.spotify.com/v1/me/player",
-    type: "GET",
-    beforeSend: (xhr) => {
-      xhr.setRequestHeader("Authorization", "Bearer " + token);
-    },
-    success: (data) => {
-      this.setState({
-        item: data.item,
-        is_playing: data.is_playing,
-        progress_ms: data.progress_ms,
-      });
-    }
-  });
-}
-  
-  
+  getCurrentlyPlaying(token) {
+    // Make a call using the token
+    $.ajax({
+      url: "https://api.spotify.com/v1/me/player",
+      type: "GET",
+      beforeSend: (xhr) => {
+        xhr.setRequestHeader("Authorization", "Bearer " + token);
+      },
+      success: (data) => {
+        this.setState({
+          item: data.item,
+          is_playing: data.is_playing,
+          progress_ms: data.progress_ms,
+        });
+      },
+    });
+  }
 
-  randomArray(){
-    const rectangles = []
+  randomArray() {
+    const rectangles = [];
     // change the number of bars if needed
-    for(let i = 0; i < 16; i++){
+    for (let i = 0; i < 16; i++) {
       const val = getRandomNum(100, 500);
       rectangles.push(Math.floor(val));
     }
 
-    this.setState({rectangles: rectangles});
+    this.setState({ rectangles: rectangles });
   }
 
-  updateOneBar(i){
+  updateOneBar(i) {
     // create a copy of the array
     const rect = this.state.rectangles.slice();
     rect[i] = Math.floor(getRandomNum(100, 500));
 
-    this.setState({rectangles: rect});
+    this.setState({ rectangles: rect });
   }
 
-  render(){
+  render() {
     const array = this.state.rectangles;
     const colors = this.state.colors;
-    const button = 'Generate Array';
+    const button = "Generate Array";
 
     return (
       <div>
         <div className="bar-parent">
           {array.map((value, index) => {
             const styles = {
-              height: `${value}px` ,
+              height: `${value}px`,
               backgroundColor: `${colors[index]}`,
-            }
-            return <div className="bar" key={index} style={ styles }></div>
-            })
-          }
-        
+            };
+            return <div className="bar" key={index} style={styles}></div>;
+          })}
         </div>
         <div className="parent">
           <Form
-          onClick={() => this.updateOneBar(Math.floor(getRandomNum(0, array.length - 1)))}
-          value={button}
+            onClick={() =>
+              this.updateOneBar(Math.floor(getRandomNum(0, array.length - 1)))
+            }
+            value={button}
           />
         </div>
         <div className="App">
-      <header className="App-header">
-      {/* <img src={"spotify.png"} className="App-logo" alt="logo" /> */}
-      {!this.state.token && (
-        <a
-          className="btn btn--loginApp-link"
-          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
-        >
-          Login to Spotify
-        </a>
-      )}
-      {this.state.token && (
-        <Player
-        item={this.state.item}
-        is_playing={this.state.is_playing}
-        progress_ms={this.progress_ms}
-      />
-    )}
-      </header>
-    </div>
-      </div> 
-      
-    )
+          <header className="App-header">
+            {/* <img src={"spotify.png"} className="App-logo" alt="logo" /> */}
+            {!this.state.token && (
+              <a
+                className="btn btn--loginApp-link"
+                href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+                  "%20"
+                )}&response_type=token&show_dialog=true`}
+              >
+                Login to Spotify
+              </a>
+            )}
+            {this.state.token && (
+              <Player
+                item={this.state.item}
+                is_playing={this.state.is_playing}
+                progress_ms={this.progress_ms}
+              />
+            )}
+          </header>
+        </div>
+      </div>
+    );
   }
 }
 
 // get a value between the specified values
 // add a Math.floor here
-function getRandomNum(min, max){
+function getRandomNum(min, max) {
   return Math.random() * (max - min) + min;
 }
 
--- src/Player.js
+++ src/Player.js
@@ -1,15 +1,13 @@
 import React from "react";
 import "./Player.css";
 
-const Player = props => {
+const Player = (props) => {
   const backgroundStyles = {
-    backgroundImage:`url(${
-      props.item.album.images[0].url
-    })`,
+    backgroundImage: `url(${props.item.album.images[0].url})`,
   };
 
   const progressBarStyles = {
-    width: (props.progress_ms * 100 / props.item.duration_ms) + '%'
+    width: (props.progress_ms * 100) / props.item.duration_ms + "%",
   };
 
   return (
@@ -34,6 +32,6 @@ const Player = props => {
       </div>
     </div>
   );
-}
+};
 
 export default Player;
