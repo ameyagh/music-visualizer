0139c24328bdb47672409cc359533e37e0bd95d4
saketv2 <saketv2@illinois.edu>
2020-12-31 19:10:04 -0600

    Added prelimanary API connection

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
 import './App.css';
+import hash from "./hash";
 import Form from './components/Form';
+import * as $ from "jquery";
+import Player from "./Player";
+
+
+/**
+ * Spotify API Read
+ */
+export const authEndpoint = 'https://accounts.spotify.com/authorize';
+
+// Replace with your app's client ID, redirect URI and desired scopes
+const clientId = "df010c4f38ab4947aabe303af43358d1";
+const redirectUri = "http://localhost:3000";
+const scopes = [
+  "user-read-currently-playing",
+  "user-read-playback-state",
+];
+
 
 // make this code more modular
 // break the rectangles into a function or component
@@ -13,14 +31,52 @@ class App extends Component{
       // fix the colors later
       colors: ['red', 'orange', 'yellow', 'green', 'blue', 
       'indigo', 'violet', 'red', 'orange', 'yellow', 'green', 
-      'blue', 'indigo', 'violet', 'indigo', 'violet']
+      'blue', 'indigo', 'violet', 'indigo', 'violet'],
+      token: null,
+      item: {
+      album: {
+        images: [{ url: "" }]
+      },
+      name: "",
+      artists: [{ name: "" }],
+      duration_ms:0,
+    },
+      is_playing: "Paused",
+      progress_ms: 0
     };
+    this.getCurrentlyPlaying = this.getCurrentlyPlaying.bind(this);
   }
-
-  // setup the array of rectangles
-  componentDidMount(){
+// setup the array of rectangles
+componentDidMount(){
+  let _token = hash.access_token;
+  if (_token) {
+    // Set token
+    this.setState({
+      token: _token
+    });
+  }
     this.randomArray();
   }
+
+getCurrentlyPlaying(token) {
+  // Make a call using the token
+  $.ajax({
+    url: "https://api.spotify.com/v1/me/player",
+    type: "GET",
+    beforeSend: (xhr) => {
+      xhr.setRequestHeader("Authorization", "Bearer " + token);
+    },
+    success: (data) => {
+      this.setState({
+        item: data.item,
+        is_playing: data.is_playing,
+        progress_ms: data.progress_ms,
+      });
+    }
+  });
+}
+  
+  
 
   randomArray(){
     const rectangles = []
@@ -65,9 +121,27 @@ class App extends Component{
           value={button}
           />
         </div>
-        
-      </div>
-      
+        <div className="App">
+      <header className="App-header">
+      {/* <img src={"spotify.png"} className="App-logo" alt="logo" /> */}
+      {!this.state.token && (
+        <a
+          className="btn btn--loginApp-link"
+          href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join("%20")}&response_type=token&show_dialog=true`}
+        >
+          Login to Spotify
+        </a>
+      )}
+      {this.state.token && (
+        <Player
+        item={this.state.item}
+        is_playing={this.state.is_playing}
+        progress_ms={this.progress_ms}
+      />
+    )}
+      </header>
+    </div>
+      </div> 
       
     )
   }
